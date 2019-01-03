ae7e5d67bff4ec6cc9fc5094cb7c42d1e43d6b8f
nanfuka <kalungi2k6@gmail.com>
2019-01-03 14:37:01 +0300

    Add index route

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request, json
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"status": 201, "message": "hi welcome to ireporter"})
--- views.py
+++ views.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"status": 201, "message": "hi welcome to ireporter"})
+    return jsonify({"status": 201, "message": "hi wevcbvnvhglcome to ireporter"})
